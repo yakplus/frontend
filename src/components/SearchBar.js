dbc3706e42e2da4114663fdd41eb6f843b40347c
Yejeong, Ham <dev@thelightway.kr>
2025-05-01 10:35:33 +0900

    ✨ Feature/#9 약품 상세정보 페이지 (#12)
    * ✨  Feat: 약품 상세정보 페이지 구현
    
    * ✨  Feat: 약품 상세정보 페이지 백엔드 연동
    
    * ✨  Feat: 한약 구분 및 취소정보 추가
    
    * 📦️  Chore: 레이아웃 확인용 mock 객체 삭제
    
    * 🐛 Fix: useSearchParams의 Server 컴포넌트 사용 문제 해결
    
    ---------
    
    Co-authored-by: HaechangLee <112938092+HaechangLee@users.noreply.github.com>

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect, useCallback } from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 const SearchIcon = ({ color = '#2BA89C', onClick }) => (
   <button
@@ -37,16 +37,19 @@ const SearchIcon = ({ color = '#2BA89C', onClick }) => (
   </button>
 );
 
-const SearchBar = ({ initialQuery = '', showTabs = true, initialMode = 'keyword' }) => {
+const SearchBar = ({ initialQuery = '', showTabs = true, initialMode = 'keyword', initialSearchType = 'symptom'}) => {
   const router = useRouter();
+  const searchParams = useSearchParams();
   const [searchMode, setSearchMode] = useState(initialMode);
-  const [searchType, setSearchType] = useState('symptom');
+  const urlType = searchParams.get('type');
+  const [searchType, setSearchType] = useState(urlType || initialSearchType);
   const [searchQuery, setSearchQuery] = useState(initialQuery);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [suggestions, setSuggestions] = useState([]);
   const [selectedSuggestion, setSelectedSuggestion] = useState(-1);
   const [isFocused, setIsFocused] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+
   const [autoCompleteCache, setAutoCompleteCache] = useState({}); // 자동완성 결과 캐시
   const MAX_CACHE_SIZE = 100;
 
@@ -67,7 +70,7 @@ const updateCache = (prevCache, key, value) => {
   const searchTypes = {
     symptom: '증상',
     company: '제조사',
-    medicine: '약품명'
+    name: '약품명'
   };
 
   const getPlaceholder = () => {
@@ -80,7 +83,7 @@ const updateCache = (prevCache, key, value) => {
         return '증상을 입력하세요';
       case 'company':
         return '제조사를 입력하세요';
-      case 'medicine':
+      case 'name':
         return '약품명을 입력하세요';
       default:
         return '검색어를 입력하세요';
@@ -116,11 +119,7 @@ const updateCache = (prevCache, key, value) => {
       // 캐시 업데이트 - 기존 결과를 새로운 결과로 덮어씌움
       const cacheKey = `${type}:${query}`;
       const results = data.data.autoCompleteList || [];
-      // setAutoCompleteCache(prev => {
-      //   const newCache = { ...prev };
-      //   newCache[cacheKey] = results; // 기존 키가 있으면 덮어씌움
-      //   return newCache;
-      // });
+
       // 캐시 무한정 커지지 않게 개수 체크하면서 추가 (MAX_CACHE_SIZE를 초과하면 가장 오래된 것 삭제)
       setAutoCompleteCache(prev => updateCache(prev, cacheKey, results));
       
@@ -132,94 +131,6 @@ const updateCache = (prevCache, key, value) => {
       setIsLoading(false);
     }
   };
-
-  // debounce된 자동완성 함수
-  // const debouncedFetchSuggestions = useCallback(
-  //   debounce(async (query, type) => {
-  //     if (query === prevQuery) {
-  //       console.log('같은 검색어, 요청 스킵:', query);
-  //       return;
-  //     }
-  //     const results = await fetchSuggestions(query, type);
-  //     setSuggestions(results.map(text => ({ text, category: searchTypes[type] })));
-  //   }, 1000),
-  //   [fetchSuggestions]
-  // );
-
-  // 포커스 상태가 변경될 때 실행
-  // useEffect(() => {
-  //   if (!isFocused) {
-  //     setSuggestions([]);
-  //     return;
-  //   }
-
-  //   // 포커스가 들어왔을 때 현재 검색어에 대한 캐시 확인
-  //   const trimmedQuery = searchQuery.trim();
-  //   if (searchMode === 'keyword' && trimmedQuery.length > 0) {
-  //     const cacheKey = `${searchType}:${trimmedQuery}`;
-  //     if (autoCompleteCache[cacheKey]) {
-  //       console.log('포커스 시 캐시된 결과 사용:', cacheKey);
-  //       setSuggestions(autoCompleteCache[cacheKey].map(text => ({ text, category: searchTypes[searchType] })));
-  //     }
-  //     // 포커스 시에는 API 요청을 보내지 않음
-  //   }
-  // }, [isFocused]);
-
-  // // 검색어나 타입이 변경될 때 실행
-  // useEffect(() => {
-  //   if (!isFocused) return;
-
-  //   const trimmedQuery = searchQuery.trim();
-  //   if (searchMode === 'keyword' && trimmedQuery.length > 0) {
-  //     const cacheKey = `${searchType}:${trimmedQuery}`;
-  //     if (autoCompleteCache[cacheKey]) {
-  //       console.log('검색어 변경 시 캐시된 결과 사용:', cacheKey);
-  //       setSuggestions(autoCompleteCache[cacheKey].map(text => ({ text, category: searchTypes[searchType] })));
-  //     } else {
-  //       debouncedFetchSuggestions(trimmedQuery, searchType);
-  //     }
-  //   } else {
-  //     setSuggestions([]);
-  //   }
-  // }, [searchQuery, searchType, searchMode, autoCompleteCache]);
-
-  // useEffect(() => {
-  //   if (!isFocused) return;
-  //   const trimmedQuery = searchQuery.trim();
-  //   if (searchMode === 'keyword' && trimmedQuery.length > 0) {
-  //     const cacheKey = `${searchType}:${trimmedQuery}`;
-  //     if (autoCompleteCache[cacheKey]) {
-  //       console.log('검색어 변경 시 캐시된 결과 사용:', cacheKey);
-  //       setSuggestions(autoCompleteCache[cacheKey].map(text => ({ text, category: searchTypes[searchType] })));
-  //     } else if (trimmedQuery !== prevQuery) {
-  //       console.log('검색어 변경 시 서버 재요청:', trimmedQuery);
-  //       debouncedFetchSuggestions(trimmedQuery, searchType);
-  //       setPrevQuery(trimmedQuery);
-  //     }
-  //   } else {
-  //     setSuggestions([]);
-  //   }
-  // }, [searchQuery, searchType, searchMode, autoCompleteCache, isFocused, prevQuery]);
-
-  // 검색어, 타입, 모드, 포커스 변화에 따라 자동완성 처리
-  // useEffect(() => {
-  //   if (!isFocused) return;
-  
-  //   const trimmedQuery = searchQuery.trim();
-  //   if (searchMode === 'keyword' && trimmedQuery.length > 0) {
-  //     const cacheKey = `${searchType}:${trimmedQuery}`;
-  
-  //     if (autoCompleteCache[cacheKey]) {
-  //       console.log('검색어 변경 시 캐시 사용:', cacheKey);
-  //       setSuggestions(autoCompleteCache[cacheKey].map(text => ({ text, category: searchTypes[searchType] })));
-  //     } else {
-  //       console.log('검색어 변경 시 서버 재요청:', trimmedQuery);
-  //       debouncedFetchSuggestions(trimmedQuery, searchType);
-  //     }
-  //   } else {
-  //     setSuggestions([]);
-  //   }
-  // }, [searchQuery, searchType, searchMode, autoCompleteCache, debouncedFetchSuggestions]);
 
   useEffect(() => {
     if (!isFocused) {
@@ -298,8 +209,9 @@ const updateCache = (prevCache, key, value) => {
         case 'company':
           // 추후 구현
           break;
-        case 'medicine':
-          // 추후 구현
+        case 'name':
+          saveRecentSearch(searchQuery, searchType, searchMode);
+          router.push(`/search/name?q=${encodeURIComponent(searchQuery)}&mode=${searchMode}`);
           break;
       }
     } else {
@@ -373,10 +285,15 @@ const updateCache = (prevCache, key, value) => {
     setSearchQuery(suggestion.text);
     setSuggestions([]);
     saveRecentSearch(suggestion.text, searchType, 'keyword');
-    if (searchType === 'symptom') {
-      router.push(`/search/symptom?q=${encodeURIComponent(suggestion.text)}&mode=keyword`);
-    } else {
-      router.push(`/search?q=${encodeURIComponent(suggestion.text)}&mode=keyword`);
+    switch (searchType) {
+      case 'symptom':
+        router.push(`/search/symptom?q=${encodeURIComponent(suggestion.text)}&mode=keyword&type=${searchType}`);
+        break;
+      case 'name':
+        router.push(`/search/name?q=${encodeURIComponent(suggestion.text)}&mode=keyword&type=${searchType}`);
+        break;
+      default:
+        router.push(`/search/symptom?q=${encodeURIComponent(suggestion.text)}&mode=keyword&type=${searchType}`);
     }
   };
 
@@ -503,7 +420,7 @@ const updateCache = (prevCache, key, value) => {
                     }}
                     className={`w-full text-left px-4 py-3 hover:bg-[#2BA89C]/5 transition-colors ${
                       searchType === type ? 'text-[#2BA89C] font-medium bg-[#2BA89C]/10' : 'text-gray-700'
-                    } ${type === 'medicine' ? 'rounded-b-lg' : ''} ${
+                    } ${type === 'name' ? 'rounded-b-lg' : ''} ${
                       type === 'symptom' ? 'rounded-t-lg' : ''
                     }`}
                   >
@@ -516,12 +433,7 @@ const updateCache = (prevCache, key, value) => {
         )}
       </form>
 
-      {/* 자동완성 결과 */}
-      {searchQuery && !isLoading && autoCompleteCache[`${searchType}:${searchQuery}`] && autoCompleteCache[`${searchType}:${searchQuery}`].length > 0 && (
-        <div className="absolute z-10 w-full bg-white mt-1 rounded-lg shadow-lg border border-gray-200">
-          {/* ... existing autocomplete results code ... */}
-        </div>
-      )}
+      
     </div>
   );
 };
--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams, useRouter } from 'next/navigation';
 import { useState, useEffect } from 'react';
 import Header from './Header';
 import Footer from './Footer';
@@ -9,8 +9,9 @@ import SearchBar from './SearchBar';
 
 const SearchPage = ({ searchType }) => {
   const searchParams = useSearchParams();
+  const router = useRouter();
   const query = searchParams.get('q');
-  const mode = searchParams.get('mode') || 'keyword';
+  const mode = searchParams.get('mode');
 
   const [currentPage, setCurrentPage] = useState(1);
   const [totalResults, setTotalResults] = useState(0);
@@ -19,6 +20,11 @@ const SearchPage = ({ searchType }) => {
   const [fetchedResults, setFetchedResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  // 약품 상세 페이지로 이동하는 함수
+  const navigateToDrugDetail = (drugId) => {
+    router.push(`/drugs/${drugId}`);
+  };
 
   useEffect(() => {
     if (!query) {
@@ -32,31 +38,45 @@ const SearchPage = ({ searchType }) => {
 
     let url;
     let options = { method: 'GET' };
-
-    if (searchType === 'keyword') {
+    if(mode === 'natural') { //자연어 검색
       url = '/api/api/drugs/search';
       options = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ query, page: apiPage, size: itemsPerPage })
       };
-    } else if (searchType === 'symptom') {
-      url = `/api/api/drugs/search/symptom?q=${encodeURIComponent(query)}&page=${apiPage}&size=${itemsPerPage}`;
+    } else { // 키워드 검색 모드 분기
+      if(searchType === 'symptom') { // 증상 검색
+        url = `/api/api/drugs/search/symptom?q=${encodeURIComponent(query)}&page=${apiPage}&size=${itemsPerPage}`;
+      } else if(searchType === 'name') { // 약품명 검색
+        url = `/api/api/drugs/search/name?q=${encodeURIComponent(query)}&page=${apiPage}&size=${itemsPerPage}`;
+      }
     }
 
-    fetch(url, options)
+    // if (searchType === 'natural') {
+    //   url = '/api/api/drugs/search';
+    //   options = {
+    //     method: 'POST',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify({ query, page: apiPage, size: itemsPerPage })
+    //   };
+    // } else if (searchType === 'symptom') {
+    //   url = `/api/api/drugs/search/symptom?q=${encodeURIComponent(query)}&page=${apiPage}&size=${itemsPerPage}`;
+    // }
+    
+    fetch(url, options) // 검색 타입에 따라 데이터 받아오는 방식 분기됨. !!! response 형식 정해지면 수정 필요
       .then(res => {
         if (!res.ok) throw new Error('서버 에러');
         return res.json();
       })
       .then(data => {
-        let list = searchType === 'keyword' ? data.data : data.data.searchResponseList;
+        let list = mode === 'keyword' ? data.data.searchResponseList : data.data;
         setFetchedResults(list);
         setTotalResults(list.length);
       })
       .catch(err => setError(err.message))
       .finally(() => setIsLoading(false));
-  }, [query, currentPage, searchType]);
+  }, [query, currentPage, mode]);
 
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
@@ -78,9 +98,10 @@ const SearchPage = ({ searchType }) => {
             ) : fetchedResults.length > 0 ? (
               fetchedResults.map(medicine => (
                 <div
-                key={medicine.drugId}
-                className="bg-white rounded-lg shadow-sm p-4 border border-transparent hover:shadow-md hover:border-[#2BA89C] transition"
-              >
+                  key={medicine.drugId}
+                  className="bg-white rounded-lg shadow-sm p-4 border border-transparent hover:shadow-md hover:border-[#2BA89C] transition cursor-pointer"
+                  onClick={() => navigateToDrugDetail(medicine.drugId)}
+                >
                   <div className="flex divide-x divide-gray-200">
                     {/* 이미지 영역 */}
                     <div className="w-48 h-48 flex-shrink-0 pr-4">
@@ -97,39 +118,36 @@ const SearchPage = ({ searchType }) => {
 
                     {/* 정보 영역 */}
                     <div className="flex-1 mt-1 pl-4 space-y-3 divide-y divide-gray-300">
-                    {/* 명칭 */}
-                    <div className="flex items-center space-x-2 py-2">
-                      <span className="flex-shrink-0 w-18 px-2 py-1 bg-[#2BA89C]/80 rounded font-bold text-white text-center whitespace-nowrap">
-                        명 칭
-                      </span>
-                      <span className="text-base text-gray-600 break-words">
-                        {medicine.drugName}
-                      </span>
-                    </div>
-                    {/* 제약회사 */}
-                    <div className="flex items-center space-x-2 py-2">
-                      <span className="flex-shrink-0 w-18 px-2 py-1 bg-[#2BA89C]/80 rounded font-bold text-white text-center whitespace-nowrap">
-                        제약회사
-                      </span>
-                      <span className="text-base text-gray-600 break-words">
-                        {medicine.company}
-                      </span>
-                    </div>
-                    {/* 효능 */}
-                    <div className="flex items-center space-x-2 py-2">
-                      <span className="flex-shrink-0 w-18 px-2 py-1 bg-[#2BA89C]/80 rounded font-bold text-white text-center whitespace-nowrap">
-                        효 능
-                      </span>
-                      <span className="text-base text-gray-600 break-words">
-                        {medicine.efficacy.join(', ')}
-                      </span>
+                      {/* 명칭 */}
+                      <div className="flex items-center space-x-2 py-2">
+                        <span className="flex-shrink-0 w-18 px-2 py-1 bg-[#2BA89C]/80 rounded font-bold text-white text-center whitespace-nowrap">
+                          명 칭
+                        </span>
+                        <span className="text-base text-gray-600 break-words">
+                          {medicine.drugName}
+                        </span>
+                      </div>
+                      {/* 제약회사 */}
+                      <div className="flex items-center space-x-2 py-2">
+                        <span className="flex-shrink-0 w-18 px-2 py-1 bg-[#2BA89C]/80 rounded font-bold text-white text-center whitespace-nowrap">
+                          제약회사
+                        </span>
+                        <span className="text-base text-gray-600 break-words">
+                          {medicine.company}
+                        </span>
+                      </div>
+                      {/* 효능 */}
+                      <div className="flex items-center space-x-2 py-2">
+                        <span className="flex-shrink-0 w-18 px-2 py-1 bg-[#2BA89C]/80 rounded font-bold text-white text-center whitespace-nowrap">
+                          효 능
+                        </span>
+                        <span className="text-base text-gray-600 break-words">
+                          {medicine.efficacy.join(', ')}
+                        </span>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              
-                
-
               ))
             ) : (
               <div className="text-center py-16">
