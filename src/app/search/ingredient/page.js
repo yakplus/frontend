53d7deb4e7014e4049a42e6ad0d5a4a8b950771c
HaechangLee <112938092+HaechangLee@users.noreply.github.com>
2025-05-06 02:16:48 +0900

    ✨  Feat: 성분검색 기능 라우팅 정상화 (#27)

--- src/app/search/ingredient/page.js
+++ src/app/search/ingredient/page.js
@@ -6,7 +6,7 @@ import SearchPage from '../../../components/SearchPage';
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <SearchPage searchType="material" />
+      <SearchPage searchType="ingredient" />
     </Suspense>
   );
 } 
